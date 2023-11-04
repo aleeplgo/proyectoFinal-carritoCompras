99edafbde8eff43879fcbec93558fc02fd7f75e7
aleeplgo <m4estudio.qro@gmail.com>
2023-11-04 13:41:35 -0600

    Agregue contador acumulador y lo muestro en el icono de carrito

--- scripts/agregarAlCarrito.js
+++ scripts/agregarAlCarrito.js
@@ -1,10 +1,12 @@
 /*VARIABLES*/
 const btnAgregarCarrito = document.querySelectorAll(".agregar-carrito"); 
+const numeritoContador = document.querySelector(".contador"); 
+console.log(numeritoContador);
 eventsToCart(); 
 
 /*EVENTOS*/
 function eventsToCart(){
-    console.log(btnAgregarCarrito);
+    //console.log(btnAgregarCarrito);
     //Cuando son varios botones se agrega: 
     //Esto se hace porque cuando seleccionas diferentes elementos con la misma clase se hace un arreglo NodeList y hay que recorrerlo
     for(const btn of btnAgregarCarrito){
@@ -17,10 +19,12 @@ function agregarAlCarrito(e){
     e.preventDefault(); 
 
     if(btnAgregarCarrito){
+
+        //Librería de notificaciones 
         Toastify({
             text: "Producto Agregado",
             duration: 3000,
-            destination: "carrito.html",
+            //destination: "carrito.html",
             newWindow: true,
             close: true,
            // gravity: "top", // `top` or `bottom`
@@ -32,7 +36,20 @@ function agregarAlCarrito(e){
             },
             onClick: function(){} // Callback after click
           }).showToast();
-    }     
+
+            // Hacer el incremento del contador
+            numeritoContador.classList.remove("inactive");
+            numeritoContador.classList.add("active"); 
+            let contador = sumarContador();
+            numeritoContador.textContent = contador;
+
+    } 
     
     console.log("Producto agregado al carrito");
+}
+
+function sumarContador() {
+    let suma = Number(numeritoContador.textContent) || 0; // Obtén el valor actual del contador y conviértelo a un número
+    suma += 1;
+    return suma;
 }
