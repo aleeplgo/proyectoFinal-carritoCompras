71fa4bc2d0df73f325a145043b53aaa3f787475d
aleeplgo <m4estudio.qro@gmail.com>
2023-11-04 09:43:35 -0600

    Se implementó la librería sweet alert para el formulario de búsqueda de la página de inicio

---
+++ scripts/buscarProdcto.js
@@ -0,0 +1,35 @@
+const formularioBuscador = document.querySelector("#formularioBuscador"); 
+const inptSearch = document.querySelector("#inptSearch"); 
+const btnSearch = document.querySelector("#btnSearch"); 
+
+//Eventos
+eventsFormSearch();
+function eventsFormSearch(){
+    formularioBuscador.addEventListener("submit", buscarProducto); 
+}
+
+
+function buscarProducto(e){
+    e.preventDefault(); 
+    console.log(inptSearch.value.trim());
+    if(inptSearch.value !== ""){
+        Swal.fire({
+            title: `${inptSearch.value.trim()}`,
+            text: `¡Está disponible en el catálogo!`,
+            icon: 'success',
+          }); 
+          limpiarInput(); 
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops... Algo salió mal',
+            text: 'Recuerda que no puedes dejar campos vacíos para poder implementar la búsqueda',
+            footer: '<a href="">¿Por qué tengo este problema?</a>'
+        })
+}
+}
+
+
+function limpiarInput(){
+    inptSearch.value=""; 
+}
