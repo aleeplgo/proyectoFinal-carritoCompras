56e521a10e5e6ebe81b6e3a270a216e28755874f
aleeplgo <m4estudio.qro@gmail.com>
2023-11-04 10:48:46 -0600

    Se agregó librería toastify y archivo js para las notificaciones de agregar al carrito

---
+++ scripts/agregarAlCarrito.js
@@ -0,0 +1,38 @@
+/*VARIABLES*/
+const btnAgregarCarrito = document.querySelectorAll(".agregar-carrito"); 
+eventsToCart(); 
+
+/*EVENTOS*/
+function eventsToCart(){
+    console.log(btnAgregarCarrito);
+    //Cuando son varios botones se agrega: 
+    //Esto se hace porque cuando seleccionas diferentes elementos con la misma clase se hace un arreglo NodeList y hay que recorrerlo
+    for(const btn of btnAgregarCarrito){
+        btn.addEventListener("click", agregarAlCarrito); 
+    }
+}
+
+
+function agregarAlCarrito(e){
+    e.preventDefault(); 
+
+    if(btnAgregarCarrito){
+        Toastify({
+            text: "Producto Agregado",
+            duration: 30000000,
+            destination: "carrito.html",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "var(--purple)",
+              color: "var(--white)"
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+    }     
+    
+    console.log("Producto agregado al carrito");
+}
